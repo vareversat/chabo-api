3a6da0cc7e709cafdcb3b901b08fa6b2baf67644
Valentin REVERSAT <dev@vareversat.fr>
2023-08-02 22:48:08 +0200

    chore: add missing doc

--- internal/utils/controller.go
+++ internal/utils/controller.go
@@ -12,6 +12,8 @@ import (
 	"github.com/vareversat/chabo-api/internal/models"
 )
 
+// Get the int param paramName passed into the request.
+// Return an erro if not specified or empty, the value either
 func GetIntParams(c *gin.Context, paramName string) (int, error) {
 
 	paramValue, paramValueExists := c.GetQuery(paramName)
@@ -25,6 +27,8 @@ func GetIntParams(c *gin.Context, paramName string) (int, error) {
 
 }
 
+// Get the string param paramName passed into the request.
+// Return empty string if not specified or empty, the value either
 func GetStringParams(c *gin.Context, paramName string) string {
 
 	paramValue, _ := c.GetQuery(paramName)
@@ -33,7 +37,9 @@ func GetStringParams(c *gin.Context, paramName string) string {
 
 }
 
-func GetTimezone(c *gin.Context) (*time.Location, error) {
+// Get the timezone passed into the request header.
+// Return an error if the header is missing and/or malformated. The value either
+func GetTimezoneFromHeader(c *gin.Context) (*time.Location, error) {
 
 	location, err := time.LoadLocation(c.GetHeader("Timezone"))
 
@@ -45,7 +51,8 @@ func GetTimezone(c *gin.Context) (*time.Location, error) {
 
 }
 
-func GetMetadaLinks(itemCount int, limit int, offset int, path string) []interface{} {
+// Create the metadalinks associated to the response
+func ComputeMetadaLinks(itemCount int, limit int, offset int, path string) []interface{} {
 	var links []interface{}
 
 	links = append(links, models.OpenAPISelfLink{Self: models.OpenAPILink{Link: path}})
