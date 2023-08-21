b7c81b4a3ba33708451881ad33e3b09187a77772
Valentin REVERSAT <dev@vareversat.fr>
2023-08-21 22:26:32 +0200

    chore(tests): add tests for slice.go

--- internal/utils/slice.go
+++ internal/utils/slice.go
@@ -1,8 +1,8 @@
 package utils
 
 // Check if a slice of stringn contains a specified string
-func contains(s []string, str string) bool {
-	for _, v := range s {
+func contains(slice []string, str string) bool {
+	for _, v := range slice {
 		if v == str {
 			return true
 		}
@@ -14,13 +14,13 @@ func contains(s []string, str string) bool {
 // Remove an element of a string slice
 // Return the shrinked slice
 func remove(slice []string, element string) []string {
-	i := indexOf(element, slice)
+	i := indexOf(slice, element)
 	return append(slice[:i], slice[i+1:]...)
 }
 
 // Get the index of a element in a string slice
 // Return the index or -1 if not found
-func indexOf(element string, slice []string) int {
+func indexOf(slice []string, element string) int {
 	for k, v := range slice {
 		if element == v {
 			return k
---
+++ internal/utils/slice_test.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestContainsOK(t *testing.T) {
+	s := []string{"toto"}
+	want := true
+	value := contains(s, "toto")
+	if want != value {
+		t.Fatalf(`contains("toto") = %t, want match for %t`, value, want)
+	}
+}
+
+func TestContainsNOK(t *testing.T) {
+	s := []string{"toto"}
+	want := true
+	value := contains(s, "tutu")
+	if want != value {
+		t.Fatalf(`contains("toto") = %t, want match for %t`, value, want)
+	}
+}
+
+func TestRemoveIOne(t *testing.T) {
+	s := []string{"toto", "tutu"}
+	want := []string{"toto"}
+	value := remove(s, "tutu")
+	if !reflect.DeepEqual(want, value) {
+		t.Fatalf(`remove("tutu") = %q, want match for %#q`, value, want)
+	}
+
+}
+
+func TestRemoveITwo(t *testing.T) {
+	s := []string{"tutu"}
+	want := []string{}
+	value := remove(s, "tutu")
+	if !reflect.DeepEqual(want, value) {
+		t.Fatalf(`remove("tutu") = %q, want match for %#q`, value, want)
+	}
+}
+
+func TestIndexOfOK(t *testing.T) {
+	s := []string{"tutu"}
+	want := 0
+	value := indexOf(s, "tutu")
+	if want != value {
+		t.Fatalf(`indexOf("tutu") = %d, want match for %d`, value, want)
+	}
+}
+
+func TestIndexOfNOK(t *testing.T) {
+	s := []string{"tutu"}
+	want := -1
+	value := indexOf(s, "toto")
+	if want != value {
+		t.Fatalf(`indexOf("tutu") = %d, want match for %d`, value, want)
+	}
+}
