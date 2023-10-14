13ae280473e07a478555f18c35ac795938697f28
Valentin REVERSAT <dev@vareversat.fr>
2023-10-14 16:28:55 +0000

    chean: remove unused resources

--- internal/domains/api_response_domain.go
+++ internal/domains/api_response_domain.go
@@ -3,7 +3,3 @@ package domains
 type APIErrorResponse struct {
 	Error string `json:"error" example:"error in params"`
 }
-
-type APIOKResponse struct {
-	Message string `json:"message" example:"ok"`
-}
